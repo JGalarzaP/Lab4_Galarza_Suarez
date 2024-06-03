607728b8435a4ea5b11310841a75e7ecac460487
JGalarzaP <jairo.galarzap25@ucuenca.edu.ec>
2024-06-03 02:12:08 -0500

    FInalReplace

--- src/modulo.c
+++ src/modulo.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <Arduino.h>
 #include <stdio.h>
 
@@ -15,21 +14,3 @@ _Bool leer_btn(uint8_t btn_pin){
     return btn_state;
 }
 
-=======
-#include <Arduino.h>
-#include <stdio.h>
-
-void parpadear_led(uint8_t led_pin){
-    digitalWrite(led_pin,!digitalRead(led_pin));
-}
-
-void led_state(uint8_t led_pin, _Bool state){
-    digitalWrite(led_pin,state);
-}
-
-_Bool leer_btn(uint8_t btn_pin){
-    _Bool btn_state=digitalRead(btn_pin);
-    return btn_state;
-}
-
->>>>>>> 40d907843038021b5ae56e52ee7c6c180ac9f21b
