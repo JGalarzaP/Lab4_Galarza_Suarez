134eec6637a7b05b85aecbcdfa13b481d04d9054
JGalarzaP <jairo.galarzap25@ucuenca.edu.ec>
2024-06-24 04:43:37 -0500

    timedelay

--- src/modulo.c
+++ src/modulo.c
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <stdio.h>
 
+
+
 void parpadear_led(uint8_t led_pin){
     digitalWrite(led_pin,!digitalRead(led_pin));
 }
@@ -9,8 +11,23 @@ void led_state(uint8_t led_pin, _Bool state){
     digitalWrite(led_pin,state);
 }
 
-_Bool leer_btn(uint8_t btn_pin){
+_Bool nMEF(int16_t time_delay){ //Retardo no Bloqueante
+    int16_t time_base=1;
+    static int16_t count = 0;
+    int16_t n_time = time_delay / time_base; //# de Muestras por pausa
+    if (count>=n_time){
+        count = 0;
+        return true;
+    }else{
+        delay(time_base); //1ms de pausa repetida <count> veces
+        count++;
+        return false;
+    }
+}
+
+_Bool leer_btn(uint8_t btn_pin){ 
     _Bool btn_state=digitalRead(btn_pin);
     return btn_state;
 }
+
 
